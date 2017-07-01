b6c6a5497985ca359ee33c59163699cbed9d8017
AnaTofuZ <anatofuz@gmail.com>
2017-07-01 18:03:50 +0900

    init git

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::TeruTeru::Bouzu - It's new $module
+
+# SYNOPSIS
+
+    use Acme::TeruTeru::Bouzu;
+
+# DESCRIPTION
+
+Acme::TeruTeru::Bouzu is ...
+
+# LICENSE
+
+Copyright (C) AnaTofuZ.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+AnaTofuZ <anatofuz@gmail.com>
---
+++ lib/Acme/TeruTeru/Bouzu.pm
@@ -0,0 +1,53 @@
+package Acme::TeruTeru::Bouzu;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+our $teruterubouzu=<<'EOF';
+明日､天気になぁ～れ!
+
+       ｜
+       ｜
+       ｜
+     ／⌒ ＼
+    ｜ﾟ＿ﾟ｜
+     ＼＿／
+     ／￣＼
+    /  /｜ ヽ
+    L／ /＼_|
+     ＼/＼/
+EOF
+
+
+1;
+__END__
+
+=encoding utf-8
+   
+=head1 NAME
+
+Acme::TeruTeru::Bouzu - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::TeruTeru::Bouzu;
+
+=head1 DESCRIPTION
+
+Acme::TeruTeru::Bouzu is ...
+
+=head1 LICENSE
+
+Copyright (C) AnaTofuZ.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+AnaTofuZ E<lt>anatofuz@gmail.comE<gt>
+
+=cut
